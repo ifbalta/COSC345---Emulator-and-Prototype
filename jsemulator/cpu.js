6c6bce37d66c99a31de2c0caa4b0c910021a3d41
Isabel Baltazar <cheska.baltazar@gmail.com>
2015-04-08 11:21:25 +1200

    Added skeleton code to cpu.js

---
+++ jsemulator/cpu.js
@@ -0,0 +1,21 @@
+/*
+  cpu.js - responsible for interrupt handling and instruction decoding.
+ */
+var initial_pc, initial_counter;
+var PC, counter;
+
+function main(){
+    // infinite while loop
+}
+
+function decode(){
+    // massive switch statement
+}
+
+function handleInterrupt(){
+    // decode/fetch instruction
+}
+
+function init(){
+    // initialize pointers
+}
