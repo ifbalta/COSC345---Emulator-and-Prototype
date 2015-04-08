c23da42318aeff4cf5dbc687147912c33c24de79
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-04-09 11:47:35 +1200

    Added more register notes for ARM CPU

--- jsemulator/cpu.js
+++ jsemulator/cpu.js
@@ -1,5 +1,6 @@
 /*
   cpu.js - responsible for interrupt handling and instruction decoding.
+  Emulating single core 800 mhz AMD chip
  */
 var initial_pc, initial_counter;
 var PC, counter;
@@ -19,3 +20,16 @@ function handleInterrupt(){
 function init(){
     // initialize pointers
 }
+
+/*
+	Register List:
+	1 PC
+	1 cpsr
+	5 spsr
+	30 general purpose registers
+	----------------------------
+	r0-r12 are registers
+	r13 is the Stack Pointer
+	r14 is the Link Register
+	r15 is the PC	
+*/
