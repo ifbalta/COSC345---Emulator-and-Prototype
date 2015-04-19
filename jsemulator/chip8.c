10174cf15c518deb0fa37773841edaab65eebc17
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-04-19 23:31:07 +1200

    Added schoolutils, which whill hold emalloc() Added structs to chip8

--- jsemulator/chip8.c
+++ jsemulator/chip8.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "chip8.h"
+#include "schoolutils.h"
 
 /*
  System Memory Map (Zero page?)
@@ -9,20 +10,25 @@
 	0x050-0x0A0 - font set (0-F) or 0-15
 	0x200-0xFFF - program ROM and work RAM
 */
+struct chip8{
+	unsigned short opcode;
+	unsigned char memory[4096]; /* 12-bit memory*/
+	/* 
+	Chip8 has 15 general purpose registers
+	the 16th register is the carry flag.
+	 */
+	unsigned char V[16];
 
-unsigned short opcode;
-unsigned char memory[4096]; /* 12-bit memory*/
+	unsigned short I; /* register I*/
+	unsigned short PC; /* program counter*/
 
-/* 
-Chip8 has 15 general purpose registers
-the 16th register is the carry flag.
- */
-unsigned char V[16];
+	/* Graphics stuff*/
+	unsigned char gfx[64 * 32]; /*screen of 2048 pixels*/
+	unsigned char delay_timer;
+	unsigned char sound_timer;
+}
 
-unsigned short I; /* register I*/
-unsigned short PC; /* program counter*/
-
-/* Graphics stuff*/
-unsigned char gfx[64 * 32]; /*screen of 2048 pixels*/
-unsigned char delay_timer;
-unsigned char sound_timer;
+/* Just allocate the memory to chip8*/
+chip8 chip8_new(){
+	chip8 c = emalloc(sizeof chip8);
+}
--- jsemulator/cycler.c
+++ jsemulator/cycler.c
@@ -11,7 +11,30 @@
 	Linux: ./bootup
 	Windows: bootup.exe
 */
+
+/*
+	By the way, malloc() works by getting a size_t
+	The size_t is the size of your item (like a String)
+	and if your allocating an array, the size of your item * array length
+	And if your mallocing a String, remember to go + 1, to include the NULL end.
+
+	So mallocing a String x into an array wordList:
+		emalloc((strlen(x) + 1) * sizeof wordList[0][0]);
+	Meaning: 
+		Prepare a space for a String and it's null into the
+		size of the space store into wordList[0][0].
+		Since wordList is an array of Strings, wordList[0][0]
+		is the size of a String.
+
+*/
+
+chip8 myChip8;
+
 int main(){
 	printf("Hello C!");
+	/* allocated here */
+	myChip8 = chip8_new();
+	/* deallocate here*/
+	free(myChip8);
 	return EXIT_SUCCESS;
 }
