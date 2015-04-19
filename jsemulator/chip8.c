d5d9f5f95c96734e5b5f05c5e958eeccfc962055
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-04-19 23:08:05 +1200

    Changed comments from '//' to '/**/' format Included compile command in cycler.c

--- jsemulator/chip8.c
+++ jsemulator/chip8.c
@@ -1,26 +1,28 @@
-// Chip8 emulator
+/* Chip8 emulator */
 #include <stdio.h>
 #include <stdlib.h>
-#include "chip.h"
+#include "chip8.h"
 
-//
-// System Memory Map (Zero page?)
-//	0x000-0x1FF - Interpreter 
-//	0x050-0x0A0 - font set (0-F) or 0-15
-//	0x200-0xFFF - program ROM and work RAM
-//
+/*
+ System Memory Map (Zero page?)
+	0x000-0x1FF - Interpreter 
+	0x050-0x0A0 - font set (0-F) or 0-15
+	0x200-0xFFF - program ROM and work RAM
+*/
 
 unsigned short opcode;
-unsigned char memory[4096]; // 12-bit memory
+unsigned char memory[4096]; /* 12-bit memory*/
 
-// Chip8 has 15 general purpose registers//
-// the 16th register is the carry flag.//
+/* 
+Chip8 has 15 general purpose registers
+the 16th register is the carry flag.
+ */
 unsigned char V[16];
 
-unsigned short I; // register I
-unsigned short PC; //Program Counter
+unsigned short I; /* register I*/
+unsigned short PC; /* program counter*/
 
-// Graphics stuff
-unsigned char gfx[64 * 32]; // screen of 2048 pixels
+/* Graphics stuff*/
+unsigned char gfx[64 * 32]; /*screen of 2048 pixels*/
 unsigned char delay_timer;
 unsigned char sound_timer;
--- jsemulator/cycler.c
+++ jsemulator/cycler.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "chip8.h"
+/*
+	Runs chip8 while loop
 
+	Compile command:
+	gcc -W -Wall -ansi -pedantic -O2 -lm *.c -o bootup
+	
+	To run:
+	Linux: ./bootup
+	Windows: bootup.exe
+*/
 int main(){
 	printf("Hello C!");
 	return EXIT_SUCCESS;
