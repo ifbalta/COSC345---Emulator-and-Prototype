9f9ddddb4993b308452fd31c961cd85ae28c2b3f
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-04-22 23:02:28 +1200

    Added skeleton for Dart port of chip8.js

--- emulator-v1/chip8-dart/chip8.dart
+++ emulator-v1/chip8-dart/chip8.dart
@@ -1,13 +1,37 @@
+import "opcodes.dart";
 class chip8{
   
-  var memory = []; // 4096 bits
-  var display = []; // 32 * 64 pixels
-  var v = []; // 16 general purpose registers
-  var stack = []; // 18 bit register
+  var memory = new List(4096); // 4096 bits
+  var display = new List(32 * 64); // 32 * 64 pixels
+  var v = new List(16); // 16 general purpose registers
+  var stack = new List(16); // 18 bit register
+  var keys = new List(16);
   var I; // 16 bit register. should be char.
   var PC; // Program Counter
   var SP; // Stack Pointer
   var opcode; // 2-byte opcodes --> 0xF000
+  var drawFlag = 0;
+  var delay_timer = 0;
+  var sound_timer = 0;
+  var keyPress = 0;
+  var  fontset = [
+       0xF0, 0x90, 0x90, 0x90, 0xF0, 
+       0x20, 0x60, 0x20, 0x20, 0x70,
+       0xF0, 0x10, 0xF0, 0x80, 0xF0,
+       0xF0, 0x10, 0xF0, 0x10, 0xF0,
+       0x90, 0x90, 0xF0, 0x10, 0x10,
+       0xF0, 0x80, 0xF0, 0x10, 0xF0,
+       0xF0, 0x80, 0xF0, 0x90, 0xF0,
+       0xF0, 0x10, 0x20, 0x40, 0x40,
+       0xF0, 0x90, 0xF0, 0x90, 0xF0,
+       0xF0, 0x90, 0xF0, 0x10, 0xF0,
+       0xF0, 0x90, 0xF0, 0x90, 0x90,
+       0xE0, 0x90, 0xE0, 0x90, 0xE0,
+       0xF0, 0x80, 0x80, 0x80, 0xF0,
+       0xE0, 0x90, 0x90, 0x90, 0xE0,
+       0xF0, 0x80, 0xF0, 0x80, 0xF0,
+       0xF0, 0x80, 0xF0, 0x80, 0x80
+    ];
   
   /*
    * Constructor initializes cpu.
@@ -23,7 +47,7 @@ class chip8{
       v[i] = 0;
     }
     /* clear stack */
-    for(int i = 0; i < 18; i++){
+    for(int i = 0; i < 16; i++){
       stack[i] = 0;
     }
     /* clear state */
@@ -31,6 +55,10 @@ class chip8{
     SP = 0;
     opcode = 0;
     I = 0;
+    /* load fontset */
+    for(var i = 0; i < 80; i++){
+      memory[i] = fontset[i];
+    }
   }
   void bootup(){
     print("bootup");
---
+++ emulator-v1/chip8-dart/opcodes.dart
@@ -0,0 +1,8 @@
+/* massive class for switching opcodes */
+class opcodes {
+  decode(op){
+    switch(0xF000){
+    
+    }
+  }
+}
