c52bc8807ed5c769ed4d1a00cf1ca2fe3958ff74
raeciel <raecielreyes@gmail.com>
2015-05-09 16:18:54 +1200

    Improved maze game

--- emulator-frame/mazegame.js
+++ emulator-frame/mazegame.js
@@ -1,28 +1,51 @@
 $(document).ready(function () {
-        var canvas = $("#canvas")[0];
-        var context = canvas.getContext("2d");
+        var canvas = $("#canvas")[0];;
+        var context = canvas.getContext("2d");;
         var currRectX = 8;
         var currRectY = 3;
         var mazeWidth = 310;
         var mazeHeight = 314;
         var intervalVar;
-        function drawMazeAndRectangle(rectX, rectY) {
-            makeWhite(0, 0, canvas.width, canvas.height);
-            var mazeImg = new Image();
-            mazeImg.onload = function () {
-                context.drawImage(mazeImg, 0, 0);
-                drawRectangle(rectX, rectY, "#0000FF");
-                context.beginPath();
-                context.arc(100, 150, 5, 0, 2 * Math.PI, false);
-                context.closePath();
-                context.fillStyle = '#00FF00';
-                context.fill();
-            };
-            mazeImg.src = "small.gif";
+        var mazeImg = new Image();
+        mazeImg.src = "Level1.gif";
+
+
+        function init(){
+            return setInterval(draw(currRectX,currRectY),10);
         }
-        /*
+
+
+        function draw(rectX,rectY){
+            
+            //bg
+            context.drawImage(mazeImg, 0, 0);
+            
+
+            //middle/goal
+            //context.arc(100, 150, 5, 0, 2 * Math.PI, false);
+            context.beginPath();
+            context.arc(mazeWidth/2, mazeHeight/2, 5, 0, 2 * Math.PI, false);
+            context.closePath();
+            context.fillStyle = '#00FF00';
+            context.fill();
+
+            drawRectangle(rectX,rectY,"purple");
+            
+
+
+        }
+
         function drawRectangle(x, y, style) {
-            makeWhite(currRectX, currRectY, 15, 15);
+            //makeWhite(currRectX, currRectY, 15, 15);
+            context.drawImage(mazeImg, 0, 0);
+            
+            //middle/goal
+            context.beginPath();
+            context.arc(mazeWidth/2, mazeHeight/2, 5, 0, 2 * Math.PI, false);
+            context.closePath();
+            context.fillStyle = '#00FF00';
+            context.fill();
+
             currRectX = x;
             currRectY = y;
             context.beginPath();
@@ -30,22 +53,26 @@ $(document).ready(function () {
             context.closePath();
             context.fillStyle = style;
             context.fill();
-        }*/
-        function drawRectangle(x, y, style) {
-            makeWhite(currRectX, currRectY, 15, 15);
-            currRectX = x;
-            currRectY = y;
-            context.beginPath();
-            context.rect(x, y, 15, 15);
-            context.closePath();
-            context.fillStyle = style;
-            context.fill();
         }
-        function moveRect(e) {
+
+
+        function move(e) {
             var newX;
             var newY;
             var movingAllowed;
             e = e || window.event;
+            //bg
+            // context.drawImage(mazeImg, 0, 0);
+            
+
+            // //middle/goal
+            // context.beginPath();
+            // context.arc(mazeWidth/2, mazeHeight/2, 5, 0, 2 * Math.PI, false);
+            // context.closePath();
+            // context.fillStyle = '#00FF00';
+            // context.fill();
+
+
             switch (e.keyCode) {
                 case 38:   // arrow up key
                 case 87: // W key
@@ -68,23 +95,25 @@ $(document).ready(function () {
                     newY = currRectY;
                     break;
             }
-            movingAllowed = canMoveTo(newX, newY);
-            if (movingAllowed === 1) {      // 1 means 'the rectangle can move'
-                drawRectangle(newX, newY, "#0000FF");
+
+            can_Move = canMoveTo(newX, newY);
+            if (can_Move === 1) {  // 1 means collision with black, therefore can't move
+                drawRectangle(newX, newY, "purple");
                 currRectX = newX;
                 currRectY = newY;
             }
-            else if (movingAllowed === 2) { // 2 means 'the rectangle reached the end point'
+            else if (can_Move === 2) { // rectangle has met the goal.
                 clearInterval(intervalVar);
-                makeWhite(0, 0, canvas.width, canvas.height);
-                context.font = "40px Arial";
+                make_Screen_White(0, 0, canvas.width, canvas.height);
+                context.font = "20px Arial";
                 context.fillStyle = "blue";
                 context.textAlign = "center";
                 context.textBaseline = "middle";
-                context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
-                window.removeEventListener("keydown", moveRect, true);
+                context.fillText("Goal!", canvas.width / 2, canvas.height / 2);
+                window.removeEventListener("keydown", move, true);
             }
         }
+
         function canMoveTo(destX, destY) {
             var imgData = context.getImageData(destX, destY, 15, 15);
             var data = imgData.data;
@@ -106,50 +135,17 @@ $(document).ready(function () {
             }
             return canMove;
         }
-        function createTimer(seconds) {
-            intervalVar = setInterval(function () {
-                makeWhite(mazeWidth, 0, canvas.width - mazeWidth, canvas.height);
-                if (seconds === 0) {
-                    clearInterval(intervalVar);
-                    window.removeEventListener("keydown", moveRect, true);
-                    makeWhite(0, 0, canvas.width, canvas.height);
-                    context.font = "40px Arial";
-                    context.fillStyle = "red";
-                    context.textAlign = "center";
-                    context.textBaseline = "middle";
-                    context.fillText("Time's up!", canvas.width / 2, canvas.height / 2);
-                    return;
-                }
-                context.font = "20px Arial";
-                if (seconds <= 10 && seconds > 5) {
-                    context.fillStyle = "orangered";
-                }
-                else if (seconds <= 5) {
-                    context.fillStyle = "red";
-                }
-                else {
-                    context.fillStyle = "green";
-                }
-                context.textAlign = "center";
-                context.textBaseline = "middle";
-                var minutes = Math.floor(seconds / 60);
-                var secondsToShow = (seconds - minutes * 60).toString();
-                if (secondsToShow.length === 1) {
-                    secondsToShow = "0" + secondsToShow; // if the number of seconds is '5' for example, make sure that it is shown as '05'
-                }
-                context.fillText(minutes.toString() + ":" + secondsToShow, mazeWidth + 30, canvas.height / 2);
-                seconds--;
-            }, 1000);
-        }
-        function makeWhite(x, y, w, h) {
+
+        function make_Screen_White(x, y, w, h) {
             context.beginPath();
             context.rect(x, y, w, h);
             context.closePath();
             context.fillStyle = "white";
             context.fill();
         }
-        drawMazeAndRectangle(200, 3);
-        window.addEventListener("keydown", moveRect, true);
-        createTimer(300); // 2 minutes
+        //drawMazeAndRectangle(200, 3);
+        init();
+        window.addEventListener("keydown", move, true);
+        //createTimer(300); // 2 minutes
 
 });
