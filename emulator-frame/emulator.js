2ccaeb4fe2ee35b02703f64d52b7265638080261
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-05-10 22:39:41 +1200

    Completed description, feature list, installation instructions and a base line for User and Developer instructions. Also changed emulator.html title.

--- emulator-frame/emulator.js
+++ emulator-frame/emulator.js
@@ -2,16 +2,6 @@
 var runningScript;
 var protoRegex = new RegExp("prototype");
 
-
-function clear(){
-	var canvas = $("#canvas")[0]; 
-	var ctxt = canvas.getContext("2d");
-	var width = canvas.width;
-	var height = canvas.height;
-	console.log("clearing screen");
-	ctxt.clearRect(0,0,width, height);
-}
-
 function clear(){
 	var canvas = $("#canvas")[0]; 
 	var ctxt = canvas.getContext("2d");
@@ -45,11 +35,11 @@ function bootup(filename){
 		console.log("getting script");
 		runningScript = $.getScript(filename, function(){
 		console.log("starting " + filename);
-//		if (protoRegex.test(filename)) {
+		if (protoRegex.test(filename)) {
 			console.log("creating appObject");
 			prev = new appObject();
 			prev.startApp;
-	//	}
+		}
 	});
 	console.log("fetched program");	
 }
