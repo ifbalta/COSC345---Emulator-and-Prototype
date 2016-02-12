d6391d2b73547b38b65ad99ac567d9a72dc2f35b
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2016-02-12 21:27:48 +1300

    Copied "Assignment 3" into "web deploy" folder in preparation for heroku deployment.

---
+++ web deploy/emulator.js
@@ -0,0 +1,232 @@
+/**
+   emulator.js is an IO pipeline between the browser and game.js.
+   emulator.js defines functions and objects that can be used by applications
+   to access a browser's canvas element.
+
+   emulator.js is the transpiled version of Emulator.ts.
+   emulator.js requires a server to run.
+
+   Version : 2.0.0
+
+   Dependencies:
+    jQuery 1.18.0
+*/
+
+/**
+ GameObject
+    Defines an object that can be painted onto the canvas.
+    No longer needed because phaser.io has its own game loop.
+     - Image
+     - Coordinates
+ */
+var GameObject = (function () {
+    function GameObject(x, y, spriteFile) {
+        this.initialX = x;
+        this.initialY = y;
+        this.x = x;
+        this.y = y;
+        this.sprite = new Image();
+        this.sprite.src = spriteFile;
+        this.spriteFile = spriteFile;
+    }
+    GameObject.prototype.toString = function () {
+        return "(" + this.x + "," + this.y + ") " + this.spriteFile + "!";
+    };
+    GameObject.prototype.resetPos = function () {
+        this.x = this.initialX;
+        this.y = this.initialY;
+    }
+    return GameObject;
+})();
+
+/**
+ KeyObject
+    Defines an object that represents a key.
+     - KeyCode
+     - KeyPressed
+ */
+var KeyObject = (function (){
+    function KeyObject(code, pressed) {
+        this.code = code;
+        this.pressed = pressed;
+    }
+    return KeyObject;
+})();
+
+
+var clock_cycle; // how often the emulator listens for events
+
+// image resources
+var bg = new Image();
+var images = []; // GameObject array
+
+// key maps
+var keymap = []; // KeyObject array
+var SPACEBAR;
+var LEFT_KEY;
+var RIGHT_KEY;
+var UP_KEY;
+var DOWN_KEY;
+
+// passed functions
+var mappedKeyFunction;
+
+/**
+   Allows applications to provide images
+   so that the emulator can paint them.
+*/
+function addResource (name, x, y, imgFile) {
+    images[name] = new GameObject(x, y, imgFile); // to allow access by name
+    images.push(images[name]); // to allow access through forEach
+    return images[name];   
+}
+
+/**
+   Initializes emulator.
+   Must take a background image.
+*/
+function initialize(keyFunction){
+    // map directional keys
+    keymap["spacebar"] = new KeyObject(32, false);
+    keymap["left"] = new KeyObject(37, false);
+    keymap["up"] = new KeyObject(38, false);
+    keymap["right"] = new KeyObject(39, false);
+    keymap["down"] = new KeyObject(40, false)
+    keymap.push(keymap["spacebar"]);
+    keymap.push(keymap["left"]);
+    keymap.push(keymap["right"]);
+    keymap.push(keymap["down"]);
+    keymap.push(keymap["up"]);
+    LEFT_KEY = keymap["left"].pressed;
+    RIGHT_KEY = keymap["right"].pressed;
+    UP_KEY = keymap["up"].pressed;
+    DOWN_KEY = keymap["down"].pressed;
+    SPACEBAR = keymap["spacebar"].pressed;
+    // map callbacks to listeners
+    mappedKeyFunction = keyFunction;
+}
+
+/**
+ *  Start listening for events.
+ * */
+function start() {
+    if (typeof ctxt != "undefined") {
+        paint();
+        if (typeof clock_cycle != "undefined") clearInterval(clock_cycle);
+        clock_cycle = setInterval(paint, 60);
+    }
+}
+
+/**
+ *  Paints images and resets key values.
+ */
+function paint () {
+    ctxt.drawImage(bg, 0, 0);
+    images.forEach(function (gObj) {
+        ctxt.drawImage(gObj.sprite, gObj.x, gObj.y);
+    });
+    resetKeys();
+}
+
+/**
+ * Sets the application's background image.
+ * */
+function setBG (bgFile) {
+    bg.src = bgFile;
+    bg.height = h;
+    bg.width = w;
+}
+
+/**
+ * Reset emulator to default state.
+ * */
+function resetEmulator () {
+    keymap = []; // reset keymap
+    keymap["left"] = new KeyObject(37, false);
+    keymap["up"] = new KeyObject(38, false);
+    keymap["right"] = new KeyObject(39, false);
+    keymap["down"] = new KeyObject(40, false)
+    keymap.push(keymap["left"]);
+    keymap.push(keymap["right"]);
+    keymap.push(keymap["down"]);
+    keymap.push(keymap["up"]);
+    LEFT_KEY = keymap["left"].pressed;
+    RIGHT_KEY = keymap["right"].pressed;
+    UP_KEY = keymap["up"].pressed;
+    DOWN_KEY = keymap["down"].pressed;
+}
+
+
+/**
+ * Reset key states
+ * */
+function resetKeys(){
+    keymap.forEach(function (kObj){
+        if (kObj.pressed) {
+            kObj.pressed = false;
+        }
+    });
+    LEFT_KEY = keymap["left"].pressed;
+    RIGHT_KEY = keymap["right"].pressed;
+    UP_KEY = keymap["up"].pressed;
+    DOWN_KEY = keymap["down"].pressed;
+    SPACEBAR = keymap["spacebar"].pressed;
+}
+
+/**
+ * Reset image coordinates
+ * */
+function resetImages() {
+    images.forEach ( function (gObj){
+        gObj.resetPos();
+    });
+}
+
+
+/**
+ *  Allows the application to map directional buttons to other keys.
+ *  Default key is unpressed.
+*/
+function mapKey (keyName, keyCode) {
+    keyName = keyName.toLowerCase();
+    if (typeof keymap[keyName] != "undefined") {
+        keymap[keyName].code = keyCode;
+    } else {
+        keymap[keyName] = new KeyObject(keyCode, false);
+        keymap.push(keymap[keyName]);
+    }    
+}
+
+var canvas = $("#canvas")[0];
+if (typeof canvas != "undefined"){
+    var ctxt = canvas.getContext("2d");
+    var h = canvas.height;
+    var w = canvas.width;
+}
+/**
+ * Updates key objcts.
+ * */
+window.addEventListener("keydown", function (e){
+    var e = e.which;
+    keymap.forEach(function (kObj){
+        if (kObj.code == e) {
+            kObj.pressed = true;
+        }
+    });
+    LEFT_KEY = keymap["left"].pressed;
+    RIGHT_KEY = keymap["right"].pressed;
+    UP_KEY = keymap["up"].pressed;
+    DOWN_KEY = keymap["down"].pressed;
+    if (typeof mappedKeyFunction != "undefined") {
+        mappedKeyFunction();
+    }
+});
+
+
+
+
+
+
+
+
+
---
+++ web deploy/game.js
@@ -0,0 +1,195 @@
+
+/*Creates the game*/
+var game = new Phaser.Game(320, 320, Phaser.AUTO, '', 
+    { preload: preload, create: create, update: update});
+
+
+/*Loads maps, tiles, hole, ball images*/
+function preload() {
+
+    game.load.tilemap('map', 'resources/map1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map2', 'resources/map5.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map3', 'resources/map2.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map4', 'resources/map4.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map5', 'resources/map3.json', null, Phaser.Tilemap.TILED_JSON);
+
+
+    game.load.image('blackTile', 'resources/blackTile.png');
+    game.load.image('bg', 'resources/bg.jpg');
+    game.load.image('hole2', 'resources/hole2.png');
+    game.load.image('ball', 'resources/ball2.png');
+
+}
+
+
+/*Global variables*/
+var ball;
+var map;
+var layer;
+var cursors;
+var hole;
+var i = 1; //Level counter
+
+
+/**
+ **Starts and creates the game and identifies if it's going to the next level.
+ **Destroys the game, map and tile layer after the level goes to the next one.
+ **/
+function create() {
+    game.background = game.add.tileSprite(0,0,320,320,'bg');
+    initialize(); // initialize emulator
+
+
+    if(i === 1){    
+        map = game.add.tilemap('map');
+        setup();
+
+    }else if(i < 6){       
+        destroyEverything(game,map,layer); 
+        map.destroy();
+        map = game.add.tilemap('map'+i);
+        setup();
+    }else{
+        destroyEverything(game,map,layer);
+        setup(); 
+
+    }  
+    i += 1;
+    hole.body.clearCollision(true);
+}
+
+
+/**
+ ** A function that initialises the map, tile layers, sprites, 
+ ** physics p2 game engine and collisions.
+ **
+ **/
+function setup(){
+
+    map.addTilesetImage('blackTile');    
+    layer = map.createLayer('blackTile');
+    layer.resizeWorld();
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    //  Set the tiles for collision.
+    //  Do this BEFORE generating the p2 bodies below.
+    map.setCollisionBetween(1, 32,true,layer);
+   
+
+    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+    //  This call returns an array of body objects which you can perform addition actions on if
+    //  required. There is also a parameter to control optimising the map build.
+    game.physics.p2.convertTilemap(map, layer);
+  
+
+    hole = game.add.sprite(160,160,'hole2');
+    ball = game.add.sprite(20, 20, 'ball');
+    if(i > 5){
+        winLabel(); //calls this function when the player wins the game.
+    }
+    game.physics.p2.enable(ball, false);
+    game.physics.p2.enable(hole, false);
+    hole.body.setCircle(5);
+    ball.body.setCircle(9);
+    
+    game.camera.follow(ball);
+
+    //  By default the ball will collide with the World bounds,
+    //  however because you have changed the size of the world (via layer.resizeWorld) to match the tilemap
+    //  you need to rebuild the physics world boundary as well. The following
+    //  line does that. The first 4 parameters control if you need a boundary on the left, right, top and bottom of your world.
+    //  The final parameter (false) controls if the boundary should use its own collision group or not. In this case we don't require
+    //  that, so it's set to false. But if you had custom collision groups set-up then you would need this set to true.
+    game.physics.p2.setBoundsToWorld(true, true, true, true, false);
+    cursors = game.input.keyboard.createCursorKeys();
+
+}
+
+
+/**
+ ** The purpose of this function is for updating the ball's movement
+ ** and checks if the ball is overlapping the hole
+ **/
+function update() {
+    hole.body.setZeroVelocity();
+    hole.body.setZeroRotation();
+
+    
+    if (LEFT_KEY){
+        ball.body.velocity.x = -100;
+        ball.scale.x = 1;
+    } else if (RIGHT_KEY) {
+        ball.body.velocity.x = 100;
+        ball.scale.x = -1;
+    }
+
+    if (UP_KEY) {
+        ball.body.velocity.y = -100;
+    }
+
+    if (DOWN_KEY) {
+        ball.body.velocity.y = 100;
+    }
+
+    if (checkOverlap(ball,hole)){
+
+        create();
+    
+    }
+    resetKeys();
+}
+
+
+
+/**
+ ** This function is called whenever the game has to reset the map
+ ** and tile layer.
+ **/
+function destroyEverything(game,map, layer){
+    game.physics.p2.clearTilemapLayerBodies(map, layer);
+    layer.destroy();
+}
+
+
+/**
+ ** This function is called when the player wins the game.
+ ** This shows a text which says 'Congratulations!'.
+ **/
+function winLabel(){
+    var winText;
+    var win;
+    
+    win = game.add.sprite(0,0,'bg');
+    win.inputEnabled = true;
+
+    game.winText = game.add.text(
+        game.world.centerX,
+        game.world.centerY,
+        "",
+        {
+            size: "32px",
+            fill: "#ffffff",  
+            stroke: "#000000", 
+            strokeThickness: 4,
+            align: "center",
+            wordWrap: true,
+            wordWrapWidth: win.width 
+
+        }
+    );
+    game.winText.setText("Congratulations! \n You Won!");
+    game.winText.anchor.setTo(0.5, 0.5);
+  
+}
+
+
+/**
+ ** This function is called when the ball is overlapping the hole.
+ **/
+function checkOverlap(spriteA, spriteB) {
+
+    var boundsA = spriteA.getBounds();
+    return Phaser.Rectangle.intersects(boundsA, spriteB);
+
+}
